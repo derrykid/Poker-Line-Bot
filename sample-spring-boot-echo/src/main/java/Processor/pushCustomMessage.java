13190cbbf8536aa5ad5fba12fbdf7b8a906f27c8
derrykid@github.com <tzuderry@gmail.com>
2022-04-06 19:26:44 +0800

    0.1.6 add debug players functional, test out push message

--- sample-spring-boot-echo/src/main/java/Processor/pushCustomMessage.java
+++ sample-spring-boot-echo/src/main/java/Processor/pushCustomMessage.java
@@ -8,15 +8,17 @@ import com.linecorp.bot.model.response.BotApiResponse;
 import java.util.concurrent.ExecutionException;
 
 public class pushCustomMessage {
+    private static String derryUID = "U3f3dc951e3cfb83333415a2df55f0fe1";
     public static void pushHoleCards(String userID, String holeCards) {
+
 
         final LineMessagingClient client = LineMessagingClient
                 .builder("4lHdgSOo/+RQsdLDmS3R99+HclBAXUAVFcCcgfF9FIrDzNiVWyOkfho59nsDahfnrfnkLPeVjDUkLB5Q9nj6A8WVgxMZ3DGRtsRO+hqZO6qoXzLcIKWBKvJhxkPc3Y1ok9etjDBGn7Hm1gmSEthktgdB04t89/1O/w1cDnyilFU=")
                 .build();
 
             final TextMessage textMessage = EmojiProcesser.process(holeCards);
-            final TextMessage test = new TextMessage("Test" + "Ua417bc25582edd9fec30f8b179edfab1");
-            final PushMessage pushMessage = new PushMessage("Ua417bc25582edd9fec30f8b179edfab1", test);
+            final TextMessage test = new TextMessage("push comstom message method!");
+            final PushMessage pushMessage = new PushMessage(derryUID, test);
 
         final BotApiResponse botApiResponse;
         try {
@@ -24,6 +26,8 @@ public class pushCustomMessage {
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
             System.out.println("Push message error occurs");
+            return;
         }
+        System.out.println(botApiResponse);
     }
 }
