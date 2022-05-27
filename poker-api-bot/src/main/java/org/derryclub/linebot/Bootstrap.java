5a970bb866ebdacdc7a192695411b335ba14d674
derrykid@github.com <tzuderry@gmail.com>
2022-05-27 13:49:04 +0800

    1.0.5 logback config

--- poker-api-bot/src/main/java/org/derryclub/linebot/Bootstrap.java
+++ poker-api-bot/src/main/java/org/derryclub/linebot/Bootstrap.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Bootstrap {
     public static void main(String[] args) {
-        SpringApplication.run(Bootstrap.class, args);
     }
 }
