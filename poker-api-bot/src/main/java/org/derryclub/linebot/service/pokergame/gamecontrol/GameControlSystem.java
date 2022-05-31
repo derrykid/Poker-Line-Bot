54f9e609b1732c84d619719edf7c20e4a546ad60
derrykid@github.com <tzuderry@gmail.com>
2022-05-31 19:23:30 +0800

    1.1.2 set status after gameProceed

--- poker-api-bot/src/main/java/org/derryclub/linebot/service/pokergame/gamecontrol/GameControlSystem.java
+++ poker-api-bot/src/main/java/org/derryclub/linebot/service/pokergame/gamecontrol/GameControlSystem.java
@@ -225,9 +225,9 @@ public final class GameControlSystem extends GameControl {
 
         if (isEligible) {
             playerWhoCallsCommand.setPlayerStatue(Player.PlayerStatus.CHECK);
-            log.info("{}", game.getWhoseTurnToMove());
+            log.info("{hihi}", game.getWhoseTurnToMove());
             game.setWhoseTurnToMove(game.getWhoseTurnToMove() + 1);
-            log.info("{}", game.getWhoseTurnToMove());
+            log.info("{hihi}", game.getWhoseTurnToMove());
             return allCheckedOrFolded(groupId)
                     ? gameProceed(groupId)
                     : new TextMessage("You checked");
